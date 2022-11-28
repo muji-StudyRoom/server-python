541a4ab65475ced99393fa7af56adeffda40e5a5
heejae <rhj0830@gmail.com>
2022-11-28 19:37:07 +0900

    testing cors

--- Server.py
+++ Server.py
@@ -31,7 +31,7 @@ print(ES_IP, " ## ", ES_PORT, " ## ", SPRING_IP, " ## ", SPRING_PORT, " ## ", RE
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "test key"
 #
-socketio = SocketIO(app, message_queue=f'{REDIS_IP}:{REDIS_PORT}', cors_allowed_origins="*")
+socketio = SocketIO(app, message_queue=f'{REDIS_IP}:{REDIS_PORT}')
 
 users_in_room = {}
 rooms_sid = {}
